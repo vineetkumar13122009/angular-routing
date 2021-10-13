aec144afb1338f084a8d4243f1f7a5eab82627d9
Vineet Kumar <vineetkumar13122009@gmail.com>
2021-10-13 07:16:46 +0530

    Login and Logout Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './gaurds/auth.guard';
 
 const routes: Routes = [
 {path:'login',component: LoginComponent },
 {path:'forgot-password',component: ForgotPasswordComponent},
 {path:'', redirectTo:'/login',pathMatch:'full'},
-{path:'admin', loadChildren:() => import('./modules/admin/admin.module').then((m) => m.AdminModule)},
+{path:'admin', 
+canActivate: [AuthGuard],
+loadChildren:() => import('./modules/admin/admin.module').then((m) => m.AdminModule)},
 {path:'**',component: NotFoundComponent},
 
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
+import { leadingComment } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +13,29 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
 export class LoginComponent implements OnInit {
 faLock = faLock;
 
-  constructor() { }
+loginForm = new FormGroup({
+  email: new FormControl(''),
+  password: new FormControl('',)
+});
+
+  constructor(private auth: AuthService,private router: Router) { }
 
   ngOnInit(): void {
+    if (this.auth.isLoggedIn()){
+      this.router.navigate(['admin']);
+    }
   }
 
+  onSubmit(): void{
+    if(this.loginForm.value){
+      this.auth.login(this.loginForm.value).subscribe(
+        (result)=>{
+          this.router.navigate(['admin']);
+        },
+        (err:Error)=>{
+          alert(err.message);
+        }
+      );
+    }
+  }
 }
